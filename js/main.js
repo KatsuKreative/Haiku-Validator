fa228e72b99a024227e3c4c6c993532f2f4198b4
Natalie Katsu Matsui <katsukreative@gmail.com>
2022-04-12 00:19:27 -0600

    syllable count shows up on page

--- js/main.js
+++ js/main.js
@@ -3,23 +3,51 @@ document.querySelector('button').addEventListener('click', checkIfHaiku)
 
 function checkIfHaiku() {
 
-    let line1 = document.querySelector('.line1').value
-    let line2 = document.querySelector('.line2').value
-    let line3 = document.querySelector('.line3').value
+    let haikuArr = [];
 
-    let url = new URL('words?','https://api.datamuse.com/words')
-        
-    url.searchParams.append('sp', line1)
-    console.log(url.toString())
+    haikuArr.push(document.querySelector('.line1'))
+    haikuArr.push(document.querySelector('.line2'))
+    haikuArr.push(document.querySelector('.line3'))
+
+    // let line1 = document.querySelector('.line1').value
+    // let line2 = document.querySelector('.line2').value
+    // let line3 = document.querySelector('.line3').value
+
+
+    let url = new URL('/words','https://api.datamuse.com')
+    
+    haikuArr.forEach((element, index) => {
+        url.searchParams.set('sp', element.value)
+        url.searchParams.set('qe', 'sp')
+        url.searchParams.set('md', 's')
+        console.log(url.toString())
+
+        //update it to map to the line number, it doesn't do that right now
+        //template literal? and connect it with HTML
+    
+        fetch(url.toString())
+            .then (res => res.json())
+            .then (data => {
+                document.querySelector(`#line${index+1}Syllables`).innerText = data[0].numSyllables
+            })
+
+
+    })
+    
     // put user form input into values
     // maybe create an array with the strings from the input and then map them into syllables?
+    //loop over array [line1, line2, line3]
+    //make a loop to iterate over each line
 
     // fetch('https://api.datamuse.com/words?sp=this%20is%20a%20fake%20haiku&qe=sp&md=s')
     
-fetch(url.toString())
-    .then (res => res.json())
-    .then (data => {
-        console.log(data[0].word + " num syllables:" + data[0].numSyllables);
-        console.log(data)
-    })
+//     let syllables = await fetch(url.toString())
+//     .then (res => res.json())
+//     .then (data => {
+//         console.log(data[0].word + " num syllables:" + data[0].numSyllables);
+//         console.log(data)
+//     })
+
+// })
+
 }
